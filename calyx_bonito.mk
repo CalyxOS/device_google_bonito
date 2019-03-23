3a47da567d91377f2e7e14e6cedb6be044f5da1d
Chirayu Desai <chirayudesai1@gmail.com>
2019-03-23 17:29:23 +0530

    Add initial Calyx product makefiles
    Also includes:
    commit 9de620d2e6504f9bd95a640b85aa96204178c0e9
    Author: Chirayu Desai <chirayudesai1@gmail.com>
    Date:   Thu Nov 12 02:56:03 2020 +0530
    
        calyx: Inherit device-calyx.mk
    
        Change-Id: I72da630c57cfa525cacc52702fb8a41b939d9537
        (cherry picked from commit 019215002342fb6baf95b4101e0707947fa9f5d9)
    
    commit a50b942d130fd5f9450aaa7f220935e2a1694c28
    Author: Chirayu Desai <chirayudesai1@gmail.com>
    Date:   Thu Dec 3 03:17:56 2020 +0530
    
        Add overlay-calyx to the list of overlays
    
        Change-Id: If826c756ec13d147f2f147e11beea0921f81c410
        (cherry picked from commit dd2a981240a3aaa92adffae1f913f0fe4136350a)
    
    Change-Id: I0781b6d4d74680ce7f0d860048871da7c2d43fb6
    (cherry picked from commit 6d529349d98e0e375a55ba0004a61c593d2365ac)

---
+++ calyx_bonito.mk
@@ -0,0 +1,11 @@
+# Inherit some common stuff.
+$(call inherit-product, vendor/calyx/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/bonito/device-calyx.mk)
+$(call inherit-product, device/google/bonito/aosp_bonito.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := calyx_bonito
+PRODUCT_MODEL := Pixel 3a XL
+PRODUCT_RESTRICT_VENDOR_FILES := false
---
+++ device-calyx.mk
@@ -0,0 +1,1 @@
+DEVICE_PACKAGE_OVERLAYS += device/google/bonito/overlay-calyx
