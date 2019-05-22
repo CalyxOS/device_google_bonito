e94b9f88e587e778a9391a318b67e304d5287840
Rashed Abdel-Tawab <rashed@linux.com>
2019-05-22 15:13:23 -0700

    b4s4: Add privapp permissions for Google EUICC apps
    Change-Id: I2e7bc09bac103d31132bd6ae646cb2f6d7ba8b19

--- device.mk
+++ device.mk
@@ -895,3 +895,6 @@ include hardware/google/pixel/thermal/device.mk
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml
+
+PRODUCT_COPY_FILES += \
+    device/google/bonito/permissions/privapp-permissions-aosp-extended.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-aosp-extended.xml
