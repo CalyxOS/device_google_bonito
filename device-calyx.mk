5e1a9801787f60de36795d2228d7ddad576d066a
Chirayu Desai <chirayudesai1@gmail.com>
2020-12-30 12:13:28 +0530

    The full Google Pixel experience!
    * Set the correct features to make apps such as
      Google Camera work.
    
    Thanks fuzzywave40!
    
    Change-Id: I9f04238928599d9e319584b178986c34c6a5b097

--- device-calyx.mk
+++ device-calyx.mk
@@ -4,3 +4,7 @@ DEVICE_PACKAGE_OVERLAYS += device/google/bonito/overlay-calyx
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml \
     device/google/bonito/permissions/privapp-permissions-aosp-extended.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-aosp-extended.xml
+
+# For Google Camera
+PRODUCT_COPY_FILES += \
+    device/google/bonito/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
