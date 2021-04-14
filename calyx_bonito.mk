625e5d27e802843afa50e24e1605194497a63056
Chirayu Desai <chirayudesai1@gmail.com>
2021-04-15 04:39:33 +0530

    calyx: Add vendor/google_devices/ to soong namespaces
    Change-Id: Ia6ee1f730ca7a6f838603e4dd37977cb0a46e84f
    (cherry picked from commit 0332960540b4d250c83fb18e46d12561b3abb315)

--- calyx_bonito.mk
+++ calyx_bonito.mk
@@ -9,3 +9,5 @@ $(call inherit-product, device/google/bonito/aosp_bonito.mk)
 PRODUCT_NAME := calyx_bonito
 PRODUCT_MODEL := Pixel 3a XL
 PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_SOONG_NAMESPACES += vendor/google_devices/bonito
