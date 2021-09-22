98377df4e0c03fe7cd05d358c06c05beff712bc3
Michael Bestas <mkbestas@gmail.com>
2021-09-22 23:48:01 +0300

    calyx: Override product name/fingerprint/description
    Change-Id: Ie673890e0380a1b88ecb010a085e4d3c3fff9c61

--- calyx_bonito.mk
+++ calyx_bonito.mk
@@ -10,6 +10,12 @@ PRODUCT_NAME := calyx_bonito
 PRODUCT_MODEL := Pixel 3a XL
 PRODUCT_BRAND := google
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=bonito \
+    PRIVATE_BUILD_DESC="bonito-user 11 RQ3A.210905.001 7511028 release-keys"
+
+BUILD_FINGERPRINT := google/bonito/bonito:11/RQ3A.210905.001/7511028:user/release-keys
+
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_SOONG_NAMESPACES += vendor/google_devices/bonito
