ec93e8ceff8379283ef328448b81062f09701dd0
Chirayu Desai <chirayudesai1@gmail.com>
2022-09-07 02:42:14 +0530

    Update fingerprint/build description from SP2A.220505.008
    Change-Id: I237d8c5ae77c7f94f20f11a89d48f9621671c7a0

--- calyx_bonito.mk
+++ calyx_bonito.mk
@@ -12,9 +12,9 @@ PRODUCT_BRAND := google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=bonito \
-    PRIVATE_BUILD_DESC="bonito-user 12 SP2A.220505.006 8561491 release-keys"
+    PRIVATE_BUILD_DESC="bonito-user 12 SP2A.220505.008 8782922 release-keys"
 
-BUILD_FINGERPRINT := google/bonito/bonito:12/SP2A.220505.006/8561491:user/release-keys
+BUILD_FINGERPRINT := google/bonito/bonito:12/SP2A.220505.008/8782922:user/release-keys
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
