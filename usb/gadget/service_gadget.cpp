GrapheneOS/device_google_barbet

Migrate IUsb implementation to AIDL
This change migrates IUsb implementation to AIDL.
Also, IUsb and IUsbGadget now run in its own processes
to improve stability and isolation.

Port of 3c97a6bb68f6418ee41026f3bd0024405c97cf0c

Bug: 200993386
Change-Id: I681c1ba9c4f547e3cf5dc0fa7c9ee1aaf4fd603c

usb/gadget/service_gadget.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The Android Open Source Project
+ * Copyright (C) 2022 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,10 +14,9 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.usb@1.3-service.barbet"
+#define LOG_TAG "android.hardware.usb.gadget-service.barbet"
 
 #include <hidl/HidlTransportSupport.h>
-#include "Usb.h"
 #include "UsbGadget.h"
 
 using android::sp;
@@ -29,34 +28,23 @@ using android::hardware::joinRpcThreadpool;
 // Generated HIDL files
 using android::hardware::usb::gadget::V1_1::IUsbGadget;
 using android::hardware::usb::gadget::V1_1::implementation::UsbGadget;
-using android::hardware::usb::V1_3::IUsb;
-using android::hardware::usb::V1_3::implementation::Usb;
 
 using android::OK;
 using android::status_t;
 
 int main() {
-    android::sp<IUsb> service = new Usb();
-    android::sp<IUsbGadget> service2 = new UsbGadget();
-
+    android::sp<IUsbGadget> service = new UsbGadget();
     configureRpcThreadpool(2, true /*callerWillJoin*/);
     status_t status = service->registerAsService();
-
-    if (status != OK) {
-        ALOGE("Cannot register USB HAL service");
-        return 1;
-    }
-
-    status = service2->registerAsService();
 
     if (status != OK) {
         ALOGE("Cannot register USB Gadget HAL service");
         return 1;
     }
 
-    ALOGI("USB HAL Ready.");
+    ALOGI("USB gadget HAL Ready.");
     joinRpcThreadpool();
     // Under noraml cases, execution will not reach this line.
-    ALOGI("USB HAL failed to join thread pool.");
+    ALOGI("USB gadget HAL failed to join thread pool.");
     return 1;
 }

CalyxOS/device_google_bonito

Co-authored-by: Michael Bestas <mkbestas@gmail.com>

usb/gadget/service_gadget.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 The Android Open Source Project
+ * Copyright (C) 2022 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,11 +14,9 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.usb@1.3-service.bonito"
+#define LOG_TAG "android.hardware.usb.gadget-service.bonito"
 
 #include <hidl/HidlTransportSupport.h>
-
-#include "Usb.h"
 #include "UsbGadget.h"
 
 using android::sp;
@@ -30,34 +28,23 @@ using android::hardware::joinRpcThreadpool;
 // Generated HIDL files
 using android::hardware::usb::gadget::V1_1::IUsbGadget;
 using android::hardware::usb::gadget::V1_1::implementation::UsbGadget;
-using android::hardware::usb::V1_3::IUsb;
-using android::hardware::usb::V1_3::implementation::Usb;
 
 using android::OK;
 using android::status_t;
 
 int main() {
-    android::sp<IUsb> service = new Usb();
-    android::sp<IUsbGadget> service2 = new UsbGadget();
-
+    android::sp<IUsbGadget> service = new UsbGadget();
     configureRpcThreadpool(2, true /*callerWillJoin*/);
     status_t status = service->registerAsService();
-
-    if (status != OK) {
-        ALOGE("Cannot register USB HAL service");
-        return 1;
-    }
-
-    status = service2->registerAsService();
 
     if (status != OK) {
         ALOGE("Cannot register USB Gadget HAL service");
         return 1;
     }
 
-    ALOGI("USB HAL Ready.");
+    ALOGI("USB gadget HAL Ready.");
     joinRpcThreadpool();
     // Under noraml cases, execution will not reach this line.
-    ALOGI("USB HAL failed to join thread pool.");
+    ALOGI("USB gadget HAL failed to join thread pool.");
     return 1;
 }