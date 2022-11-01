TeamWin/android_device_google_crosshatch

allow-in-power-save various Qualcomm tel components
Allow various Qualcomm telephony components to run in the background.

Bug: 77981855
Change-Id: I3393e35796d2049e6c69aab2af96915789311a40

device-common.mk
@@ -101,3 +101,7 @@ PRODUCT_COPY_FILES += \
 # Dumpstate HAL
 PRODUCT_PACKAGES += \
     android.hardware.dumpstate@1.0-service.crosshatch
+
+# whitelisted app
+PRODUCT_COPY_FILES += \
+    device/google/marlin/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml

CalyxOS/device_google_bonito

device-common.mk
@@ -100,6 +100,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.dumpstate@1.0-service.bonito
 
+# whitelisted app
+PRODUCT_COPY_FILES += \
+    device/google/bonito/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+
 # Enable retrofit dynamic partitions for all bonito
 # and sargo targets
 PRODUCT_USE_DYNAMIC_PARTITIONS := true