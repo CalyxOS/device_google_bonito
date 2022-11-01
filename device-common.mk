TeamWin/android_device_google_crosshatch

Move qti_whitelist.xml to /product
Bug: 112992125
Test: boot blueline
Change-Id: I0d4fcb2dc484a354f1c90d4a07bcbaebb4c54cf2

device-common.mk
@@ -110,7 +110,7 @@ PRODUCT_PACKAGES += \
 
 # whitelisted app
 PRODUCT_COPY_FILES += \
-    device/google/crosshatch/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+    device/google/crosshatch/qti_whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/qti_whitelist.xml
 
 PRODUCT_PACKAGES += \
     llkd

CalyxOS/device_google_bonito

device-common.mk
@@ -102,7 +102,7 @@ PRODUCT_PACKAGES += \
 
 # whitelisted app
 PRODUCT_COPY_FILES += \
-    device/google/bonito/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+    device/google/bonito/qti_whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/qti_whitelist.xml
 
 # Enable retrofit dynamic partitions for all bonito
 # and sargo targets