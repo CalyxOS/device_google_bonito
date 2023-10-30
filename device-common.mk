8a5cc5f082e4bedb47b0d53074b6d86cfd6c7d5d
Michael Bestas <mkbestas@lineageos.org>
2023-10-30 02:36:59 +0200

    b4s4: Cleanup dead targets
    Change-Id: I5f94d5f1cd00a4bf2907b0063bd997abad2b39df

--- device-common.mk
+++ device-common.mk
@@ -96,10 +96,6 @@ persist.bluetooth.a2dp_offload.cap=sbc-aac-aptx-aptxhd-ldac
 PRODUCT_COPY_FILES += \
     device/google/bonito/init.logging.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.$(PRODUCT_PLATFORM).logging.rc
 
-# Dumpstate HAL
-PRODUCT_PACKAGES += \
-    android.hardware.dumpstate@1.0-service.bonito
-
 # whitelisted app
 PRODUCT_COPY_FILES += \
     device/google/bonito/qti_whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/qti_whitelist.xml
